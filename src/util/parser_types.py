f9d79d5c4adca385f9d4d9274003ef51b92c9c36
Felix Engelmann <github@fengelmann.de>
2019-01-25 19:17:09 +0100

    0.4.0
    Add version 0.4.0:
    
    * Add Nominatim Geocoding endpoint
    
    * Make the scheduler thread a daemon thread
    
    * Add exception handling for ExpiredSignatureError
    
    * Add event model and resources
    
    * Fix moment parser to output UTC

--- src/util/parser_types.py
+++ src/util/parser_types.py
@@ -1,3 +1,4 @@
+import pytz
 from dateutil import parser
 
 
@@ -8,7 +9,7 @@ def float_or_null(s):
 
 
 def moment(s):
-    parsed = parser.parse(s)
+    parsed = parser.parse(s).astimezone(pytz.utc)
     if not parsed:
         raise ValueError('Could not parse datetime')
     return parsed
