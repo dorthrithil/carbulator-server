a97e727c7b5c90a86f58ea0d8722249e73a236b5
Felix Engelmann <f.engelmann@wizai.com>
2019-02-14 14:56:44 +0100

    Add account settings model and migration script

---
+++ src/models/acount_settings.py
@@ -0,0 +1,36 @@
+import datetime
+
+from flask_restful import fields
+
+from src.app import db
+
+
+class AccountSettingsModel(db.Model):
+    __tablename__ = 'account_settings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time_created = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    time_updated = db.Column(db.DateTime(), onupdate=datetime.datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    auto_load_parking_place_gps_location = db.Column(db.Boolean, default=False)
+
+    def persist(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_marshaller():
+        return {
+            'id': fields.Integer,
+            'time_created': fields.DateTime,
+            'time_updated': fields.DateTime,
+            'auto_load_parking_place_gps_location': fields.Boolean
+        }
+
+    @classmethod
+    def find_by_user_id(cls, user_id):
+        return cls.query.filter_by(user_id=user_id).first()
+
+# todo test creation of user -> settings must be created
+# todo test deletion fo user -> cascading?
+# todo migration script with default settings for existing users
